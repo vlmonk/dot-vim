79fbeea86e135124feb70dd49305bb3c38131eb2
Eugene Korbut <me@mvl.ru>
2025-10-01 23:19:05 +0500

    Auto-format prettier even if file in gitignore

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -3,6 +3,7 @@ local null_ls = require("null-ls");
 local sources = {
   null_ls.builtins.formatting.prettier.with({
     prefer_local = "node_modules/.bin",
+    extra_args = { "--ignore-path", "''" },
     filetypes = {
       "less", "yaml", "javascript", "javascriptreact", "graphql", "typescriptreact",
       "html", "css", "vue", "scss", "svelte", "typescript", "handlebars", "jsonc", "json"
